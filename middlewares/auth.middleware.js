20924a740c6d81c9d056340a4fde70d1fe94b60d
Saurabh Purohit <saurabhpurohit@Saurabhs-MacBook-Air.local>
2023-07-31 23:04:45 +0530

    Created user update API

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 const secretKey = require('../configs/auth.config');
+const User = require('../models/user.model');
+const constants = require('../utils/constants');
 
 verifyToken = (req, res, next) => {
     let token = req.headers['x-access-token'];
@@ -21,9 +23,39 @@ verifyToken = (req, res, next) => {
 
 };
 
+isAdmin = async (req, res, next) => {
+    const user = await User.findOne({ userId: req.userId });
+    
+    if (user && user.userType === constants.userTypes.admin) {
+        next();
+    } else {
+        res.status(403).send({
+            message: `Not authorized`
+        });
+    }
+}
+
+isAdminOrOwner = async (req, res, next) => {
+    const user = await User.findOne({ userId: req.userId });
+    
+    if (user.userType === 'admin' || user.userId === req.params.id) {
+        if (req.body.userStatus && user.userType !== 'admin') {
+            return res.status(402).send({
+                message: `Not authorized`
+            });
+        }
+        next();
+    } else {
+        res.status(403).send({
+            message: `Only admin or user is allowed`
+        });
+    }
+} 
+
 const authFunction = {
-    verifyToken: verifyToken
+    verifyToken: verifyToken,
+    isAdmin: isAdmin,
+    isAdminOrOwner: isAdminOrOwner
 }
 
 module.exports = authFunction;
-
